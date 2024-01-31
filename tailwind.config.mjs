f330ffa552e2ec958b2b6e05a14b91c5231227a3
76tebe <tubagusghiffarimaulana@gmail.com>
2024-01-31 16:06:13 +0700

    Add font to tailwind

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,84 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['"Plus Jakarta Sans"', ...defaultTheme.fontFamily.sans],
+      },
+    },
+    fontSize: {
+      "heading-1": [
+        "3rem",
+        {
+          lineHeight: "120%",
+          fontWeight: "600",
+        },
+      ],
+      "heading-2": [
+        "2.5rem",
+        {
+          lineHeight: "120%",
+          fontWeight: "600",
+        },
+      ],
+      "heading-3": [
+        "2rem",
+        {
+          lineHeight: "120%",
+          fontWeight: "600",
+        },
+      ],
+      "heading-4": [
+        "1.5rem",
+        {
+          lineHeight: "120%",
+          fontWeight: "600",
+        },
+      ],
+      "heading-5": [
+        "1.125rem",
+        {
+          lineHeight: "120%",
+          fontWeight: "600",
+        },
+      ],
+      "body-1": [
+        "1.125rem",
+        {
+          lineHeight: "180%",
+		  letterSpacing: "0.5%",
+          fontWeight: "400",
+        },
+      ],
+	  "body-2": [
+        "1rem",
+        {
+          lineHeight: "180%",
+		  letterSpacing: "0.5%",
+          fontWeight: "400",
+        },
+      ],
+	  "body-3": [
+        "0.875rem",
+        {
+          lineHeight: "160%",
+		  letterSpacing: "0.5%",
+          fontWeight: "400",
+        },
+      ],
+	  "button": [
+        "1rem",
+        {
+          lineHeight: "1.5rem",
+		  letterSpacing: "0.5px",
+          fontWeight: "600",
+        },
+      ],
+    },
+  },
+  plugins: [],
+};
