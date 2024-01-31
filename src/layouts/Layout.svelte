f8f9c7d56380758694ceec12ef2729d118378ac7
76tebe <tubagusghiffarimaulana@gmail.com>
2024-01-31 15:31:15 +0700

    Add Svelte

---
+++ src/layouts/Layout.svelte
@@ -0,0 +1,18 @@
+<script>
+    export let title;
+</script>
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8" />
+		<meta name="description" content="Astro description" />
+		<meta name="viewport" content="width=device-width" />
+		<link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+		<meta name="generator" content={Astro.generator} />
+		<title>{title}</title>
+	</head>
+	<body class="{title == 'Home' ? 'mx-[120px]' : 'mx-[200px]'} my-20">
+		<slot />
+	</body>
+</html>
